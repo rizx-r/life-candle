4603e7ad7fe894054053142c508ad5b56f7404b8
Rizx <157099086+rizx-r@users.noreply.github.com>
2025-12-17 22:31:50 +0800

    merge frontend and backend into one repository

--- backend/app/utils/bazi.py
+++
@@ -1,8 +0,0 @@
-def get_stem_polarity(pillar: str) -> str:
-    if not pillar:
-        return 'YANG'
-    first_char = pillar.strip()[0]
-    yang_stems = ['甲', '丙', '戊', '庚', '壬']
-    if first_char in yang_stems:
-        return 'YANG'
-    return 'YIN'
